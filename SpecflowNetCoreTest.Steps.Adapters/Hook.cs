42002e82c985677f10c364618d2920e4a81d7743
Gennady Verdel <godrose@gmail.com>
2018-11-23 14:05:24 +0200

    Initial commit

---
+++ SpecflowNetCoreTest.Steps.Adapters/Hook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNetCoreTest.Steps.Adapters
+{
+    [Binding]
+    public class Hook
+    {
+        [AfterTestRun]
+        public static void AfterAllScenarios()
+        {
+            Console.WriteLine("After test run");
+        }
+    }
+}
---
+++ SpecflowNetCoreTest.Steps.Adapters/StepDefinition1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNetCoreTest.Steps.Adapters
+{
+    [Binding]
+    internal sealed class StepDefinition1
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {            
+            Console.WriteLine("Arrange");
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {            
+            Console.WriteLine("Act");
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {            
+            Console.WriteLine("Assert");
+        }
+    }
+}
